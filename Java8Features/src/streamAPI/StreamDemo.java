c197e29ce01d494df7f5cef7b7afe6d2d3259768
javeed512 <javeedmohammedjaveed@gmail.com>
2024-08-23 18:02:40 +0530

    StreamAPI operators

--- Java8Features/src/streamAPI/StreamDemo.java
+++ Java8Features/src/streamAPI/StreamDemo.java
@@ -2,6 +2,7 @@ package streamAPI;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class StreamDemo {
@@ -11,11 +12,13 @@ public class StreamDemo {
 
 		List<Integer>  list = new ArrayList<Integer>();
 		
-			list.add(20);
-			list.add(50);
-			list.add(30);
-			list.add(10);
-			list.add(70);
+		list.add(20);
+		list.add(50);
+		list.add(30);
+		list.add(10);
+		list.add(70);
+		list.add(20);
+		list.add(50);
 			
 			
 				Stream<Integer> stream1 =	list.stream();
@@ -31,9 +34,25 @@ public class StreamDemo {
 		
 			stream2.filter( (Integer num)->{  return  num > 30;} )
 			       .forEach((Integer num)->{ System.out.println(num);});
+				
+			
+			Stream<Integer> stream3 =	list.stream();
+			
+			//System.out.println("limit operator");
+					//stream3.limit(3).forEach(System.out::println);
+			
+			//System.out.println("skip operator");
+			//stream3.skip(3).forEach(System.out::println);
+			
+			
+			//System.out.println("distinct operator");
+			//stream3.distinct().forEach(System.out::println);
+			
+		Integer sum=	stream3.reduce((n1,n2)->{ return n1+n2; }).orElse(0);
 					
-		
-		
+				
+					System.out.println("Sum total "+sum);
+			
 		
 	}
 
