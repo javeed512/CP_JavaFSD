f269b10817d56fc3a2053faf25764458faf7c5d9
javeed512 <javeedmohammedjaveed@gmail.com>
2024-08-29 11:06:23 +0530

    JUnit Intro , Demo,Notes,PDF

---
+++ UnitTesting_JUnit5/src/com/wipro/java/Calculator.java
@@ -0,0 +1,23 @@
+package com.wipro.java;
+
+public class Calculator { // service class
+
+	public int add(int a, int b) {
+
+		return a + b;
+
+	}
+
+	public int sub(int a, int b) {
+
+		return a - b;
+
+	}
+
+	public int mul(int a, int b) {
+
+		return a * b;
+
+	}
+
+}
